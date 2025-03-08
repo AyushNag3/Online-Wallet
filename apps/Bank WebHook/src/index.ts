98b807ef6a04030efd80758091b5361912c4e320
AyushNag3 <mnag7278@gmail.com>
2025-03-08 20:50:09 +0530

    kuch bhi

--- apps/Bank WebHook/src/index.ts
+++ apps/Bank WebHook/src/index.ts
@@ -1,21 +1,46 @@
 import  express  from "express";
-import db from  "@repo/db/index" ;
+import db from "@repo/db/index";
 
 const app = express() 
-app.post('/', (req,res) => {
+app.post('/', async(req,res) => {
    const paymentInformation = {
      token: req.body.token,
      userId : req.body.user_identifier,
      amount : req.body.amount
    } ;
-   db.balance.update({
-    where : {
-        userId : paymentInformation.userId
-    },
-    data : {
-        amount : {
-            increment : paymentInformation.amount
-        }
-    }
-   })
+   try {
+    await db.$transaction([
+        db.balance.updateMany({
+            where: {
+                userId: Number(paymentInformation.userId)
+            },
+            data: {
+                amount: {
+                    // You can also get this from your DB
+                    increment: Number(paymentInformation.amount)
+                }
+            }
+        }),
+        db.onRampTransaction.updateMany({
+            where: {
+                token: paymentInformation.token
+            }, 
+            data: {
+                status: "Success",
+            }
+        })
+    ]);
+
+    res.json({
+        message: "Captured"
+    })
+} catch(e) {
+    console.error(e);
+    res.status(411).json({
+        message: "Error while processing webhook"
+    })
+}
+
 })
+
+app.listen(3003);
