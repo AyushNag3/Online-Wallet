2bb1d214004c6a69c86a99ba01607a127a922854
AyushNag3 <mnag7278@gmail.com>
2025-03-16 01:51:10 +0530

    changes

--- apps/user-app/app/lib/actions/p2ptransfer.ts
+++ apps/user-app/app/lib/actions/p2ptransfer.ts
@@ -4,7 +4,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../auth";
 import prisma from "@repo/db/client";
 import { Prisma } from "@prisma/client";
-import { timeStamp } from "console";
+
 
 export async function p2pTransfer(to: string, amount: number) {
     const session = await getServerSession(authOptions);
